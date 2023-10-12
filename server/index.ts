62e9f59fe1b01655a0e7c783be058da68a4303fa
Einar <einar.nordlund@ignite.no>
2023-10-12 11:04:29 +0200

    Make placeholder function

--- server/index.ts
+++ server/index.ts
@@ -13,21 +13,10 @@ const publicProcedure = t.procedure;
 const router = t.router;
 
 const appRouter = router({
-    greeting: publicProcedure
-        // This is the input schema of your procedure
-        // 💡 Tip: Try changing this and see type errors on the client straight away
-        .input(
-            z
-                .object({
-                    name: z.string().nullish(),
-                })
-                .nullish(),
-        )
-        .query(({ input }) => {
-            // This is what you're returning to your client
+    placeholder: publicProcedure
+        .query(() => {
             return {
-                text: `hello ${input?.name ?? 'world'}`,
-                // 💡 Tip: Try adding a new property here and see it propagate to the client straight-away
+                text: "hello"
             };
         }),
 });
